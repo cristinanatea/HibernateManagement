367811e0935fdc9cce8e5035333eef64a9f7a26f
Cristina Natea <cristina.natea4@gmail.com>
2019-12-29 23:54:44 +0200

    new

---
+++ Commons/src/main/java/crud/management/commons/ProjectInfo.java
@@ -0,0 +1,41 @@
+package crud.management.commons;
+
+import java.util.List;
+
+public class ProjectInfo {
+    
+	int projectID;
+	String projectName;
+	String managerName;
+	List<String>employeeNames;
+	String status = "";
+	
+	
+	public int getProjectID() {
+		return projectID;
+	}
+	public void setProjectID(int projectID) {
+		this.projectID = projectID;
+	}
+	public String getProjectName() {
+		return projectName;
+	}
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	public String getManagerName() {
+		return managerName;
+	}
+	public void setManagerName(String managerName) {
+		this.managerName = managerName;
+	}
+	public List<String> getEmployeeNames() {
+		return employeeNames;
+	}
+	public void setEmployeeNames(List<String> employeeNames) {
+		this.employeeNames = employeeNames;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
---
+++ Commons/src/main/java/crud/management/commons/UserInfo.java
@@ -0,0 +1,60 @@
+package crud.management.commons;
+
+import java.util.List;
+
+public class UserInfo {
+	int userID;
+	String name;
+	String phoneNumber;
+	String email;
+	List<String> asignedProjects;
+	String status = "OK";
+
+	public String getStatus() {
+		return status;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<String> getAsignedProjects() {
+		return asignedProjects;
+	}
+
+	public void setAsignedProjects(List<String> asignedProjects) {
+		this.asignedProjects = asignedProjects;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
