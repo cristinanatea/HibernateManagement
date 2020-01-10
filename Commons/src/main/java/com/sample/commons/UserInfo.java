3575c39ecef8fccece933be515f116c07e7d49ef
Cristina Natea <cristina.natea4@gmail.com>
2020-01-10 17:11:45 +0200

    Login persistent

--- Commons/src/main/java/com/sample/commons/UserInfo.java
+++ Commons/src/main/java/com/sample/commons/UserInfo.java
@@ -3,10 +3,13 @@ package com.sample.commons;
 import java.util.List;
 
 public class UserInfo {
+
+
 	@Override
 	public String toString() {
 		return "UserInfo [userID=" + userID + ", name=" + name + ", phoneNumber=" + phoneNumber + ", email=" + email
-				+ ", asignedProjects=" + asignedProjects + ", status=" + status + "]";
+				+ ", acces=" + acces + ", managerEmail=" + managerEmail + ", asignedProjects=" + asignedProjects
+				+ ", status=" + status + "]";
 	}
 
 	int userID;
@@ -14,65 +17,58 @@ public class UserInfo {
 	String phoneNumber;
 	String email;
 	int acces;
+	String managerEmail;
 	List<String> asignedProjects;
 	String status = "OK";
-
-
-	public int getAcces() {
-		return acces;
-	}
-
-	public void setAcces(int acces) {
-		this.acces = acces;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
 	public int getUserID() {
 		return userID;
 	}
-
 	public void setUserID(int userID) {
 		this.userID = userID;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
+	public int getAcces() {
+		return acces;
+	}
+	public void setAcces(int acces) {
+		this.acces = acces;
+	}
+	public String getManagerEmail() {
+		return managerEmail;
+	}
+	public void setManagerEmail(String managerEmail) {
+		this.managerEmail = managerEmail;
+	}
 	public List<String> getAsignedProjects() {
 		return asignedProjects;
 	}
-
 	public void setAsignedProjects(List<String> asignedProjects) {
 		this.asignedProjects = asignedProjects;
 	}
-
+	public String getStatus() {
+		return status;
+	}
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 
 	
 }
